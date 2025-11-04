67b890fe312c486cc2b049a5957196557c816529
asifichy <asifimtiyajchowdhury@gmail.com>
2025-11-04 17:36:22 +0600

    Add Cypress configuration file to define E2E testing setup

---
+++ cypress.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    specPattern: [
+      "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+      "cypress/horizonexp/**/*.cy.{js,jsx,ts,tsx}"
+    ],
+    chromeWebSecurity: false,
+    experimentalSessionAndOrigin: true,
+  },
+});
